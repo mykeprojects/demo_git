e89df57872d95f581a38a38955d4b0d45c4aaeb3
Sergio <sergio.arango63723@ucaldas.edu.co>
2026-03-04 17:17:37 -0500

    Cambiando nombresito

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Esto es una prueba
 print("Estoy en la universidad codificando")
 print("Estoy en la universidad estudiando")
-print("Felipe")
+print("El myke")
 print("Soyez bienvenue a la Colombie")
 print("La vida es bella?")
 print("La vida es horrible")
