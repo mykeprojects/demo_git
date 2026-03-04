7a47b2fac128ed7319ddb89d18a531d58b8a4495
Sergio <sergio.arango63723@ucaldas.edu.co>
2026-03-04 17:11:51 -0500

    En train de changer des langues

--- main.py
+++ main.py
@@ -2,6 +2,6 @@
 print("Estoy en la universidad codificando")
 print("Estoy en la universidad estudiando")
 print("Felipe")
-print("Welcome to Colombia")
+print("Soyez bienvenue a la Colombie")
 print("La vida es bella?")
 print("La vida es horrible")
